285730baf2f846812954d6761396dddb353a69a6
Vlad <vladmartishevskii@gmail.com>
2018-10-03 16:30:27 +0300

    Changing eye state

--- webapp/src/main/webapp/js/datacollector.js
+++ webapp/src/main/webapp/js/datacollector.js
@@ -1,4 +1,26 @@
-(function() {
+(function($) {
 	"use strict";
-	alert("Hello from eXo extension!");
-})();
+	
+	$(document).ready(function() {
+	 
+		$( ".actionBar" ).find( ".statusAction" ).prepend( "<li class='eye eye-default'></li>");
+
+		$('.eye').click(function() {
+
+			if($(this).hasClass("eye-default")){
+				$(this).removeClass("eye-default");
+				$(this).toggleClass('eye-relevant');
+			}
+			else if($(this).hasClass("eye-relevant")){
+				$(this).removeClass("eye-relevant");
+				$(this).toggleClass('eye-irrelevant');
+			}
+			else{
+				$(this).removeClass("eye-irrelevant");
+				$(this).toggleClass('eye-relevant');
+			}
+		});
+
+		});
+		
+})($);
