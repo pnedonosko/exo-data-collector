33d8cddfcf7e3c84f034b60e29551185a284b4d0
Peter Nedonosko <pnedonosko@exoplatform.com>
2018-10-25 17:18:57 +0300

    ActivityPost entity: querues in prigress

--- services/src/main/java/org/exoplatform/datacollector/domain/ActivityPostEntity.java
+++ services/src/main/java/org/exoplatform/datacollector/domain/ActivityPostEntity.java
@@ -2,26 +2,33 @@ package org.exoplatform.datacollector.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.NamedNativeQueries;
 import javax.persistence.NamedNativeQuery;
 
 import org.exoplatform.commons.api.persistence.ExoEntity;
 
 @Entity(name = "ActivityPost")
 @ExoEntity
-@NamedNativeQuery(
+@NamedNativeQueries({
     /* User commented someone's post */
-    name = "ActivityPost.findPartIsCommentedPoster", query = "SELECT a.activity_id AS post_id, a.provider_id AS post_provider_id, a.type AS post_type,"
-        + "  a.poster_id AS poster_id, a.owner_id AS owner_id," + "  c.posted AS c_posted_date, c.updated_date AS c_updated_date,"
-        + "  a.hidden, a.posted AS posted_date, a.updated_date" + " FROM soc_activities a, soc_activities c"
+    @NamedNativeQuery(name = "ActivityPost.findPartIsCommentedPoster", query = "SELECT a.activity_id AS post_id, a.provider_id AS post_provider_id,"
+        + "  a.type AS post_type, a.poster_id AS poster_id, a.owner_id AS owner_id, "
+        + "  c.posted AS c_posted_date, c.updated_date AS c_updated_date, a.hidden, a.posted AS posted_date, a.updated_date "
+        + " FROM soc_activities a, soc_activities c"
         + " WHERE a.activity_id = c.parent_id AND a.poster_id != c.poster_id AND a.owner_id IS NOT NULL"
-        + " AND c.poster_id = :posterId", resultClass = ActivityPostEntity.class)
+        + " AND c.poster_id = :posterId", resultClass = ActivityPostEntity.class),
+    /* User commented someone's comment in a post */
+    @NamedNativeQuery(name = "ActivityPost.findPartIsCommentedCommenter", query = "SELECT a.activity_id AS post_id, a.provider_id AS post_provider_id, "
+        + "  a.type AS post_type, oc.poster_id AS poster_id, a.owner_id AS owner_id, "
+        + "  c.posted AS c_posted_date, c.updated_date AS c_updated_date, a.hidden, a.posted AS posted_date, a.updated_date "
+        + " FROM soc_activities a, soc_activities oc, soc_activities c"
+        + " WHERE a.activity_id = oc.parent_id AND oc.activity_id = c.parent_id AND oc.poster_id != c.poster_id AND a.owner_id IS NOT NULL AND oc.owner_id IS NULL"
+        + " AND c.poster_id = :posterId", resultClass = ActivityPostEntity.class) })
 public class ActivityPostEntity {
 
   /**
    * The post ID (activity ID).
    */
-  @Id
   @Column(name = "post_id")
   protected String postId;
 
