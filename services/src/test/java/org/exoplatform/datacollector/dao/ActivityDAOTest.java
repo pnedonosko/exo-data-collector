1244137cb010d97753c1c97b716e904649c82213
Vlad Martyshevskyi <vladmartishevskii@gmail.com>
2018-11-05 18:07:54 +0200

    Attempt to create a space using Social

--- services/src/test/java/org/exoplatform/datacollector/dao/ActivityDAOTest.java
+++ services/src/test/java/org/exoplatform/datacollector/dao/ActivityDAOTest.java
@@ -2,12 +2,18 @@ package org.exoplatform.datacollector.dao;
 
 import org.junit.Test;
 
+import org.exoplatform.commons.persistence.impl.EntityManagerService;
 import org.exoplatform.commons.testing.BaseCommonsTestCase;
 import org.exoplatform.component.test.ConfigurationUnit;
 import org.exoplatform.component.test.ConfiguredBy;
 import org.exoplatform.component.test.ContainerScope;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
+import org.exoplatform.container.component.RequestLifeCycle;
+import org.exoplatform.services.security.IdentityRegistry;
+import org.exoplatform.social.core.space.impl.DefaultSpaceApplicationHandler;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
 
 @ConfiguredBy({ @ConfigurationUnit(scope = ContainerScope.ROOT, path = "conf/test-root-configuration.xml"),
     @ConfigurationUnit(scope = ContainerScope.PORTAL, path = "conf/portal/configuration.xml"),
@@ -27,7 +33,30 @@ public class ActivityDAOTest extends BaseCommonsTestCase {
     super.beforeClass();
     PortalContainer container = PortalContainer.getInstance();
     ExoContainerContext.setCurrentContainer(container);
+    RequestLifeCycle.begin(container);
+
     activityCommentDAO = (ActivityCommentedDAO) container.getComponentInstanceOfType(ActivityCommentedDAO.class);
+    SpaceService spaceService = (SpaceService) container.getComponentInstanceOfType(SpaceService.class);
+    
+    Space space = new Space();
+    
+    String[] managers = new String[] { "raul" };
+    String[] members = new String[] { "raul" };
+    
+
+    space.setDisplayName("testingSpace");
+    space.setDescription("Space for testing");
+    space.setPrettyName("testingSpace");
+    space.setGroupId("/spaces/space_test"); 
+    space.setVisibility(Space.PUBLIC);
+    space.setRegistration(Space.VALIDATION);
+    space.setPriority(Space.INTERMEDIATE_PRIORITY);
+    space.setType("classic");
+    space.setManagers(managers);
+    space.setMembers(members);
+
+    spaceService.createSpace(space, "raul");
+
   }
 
   @Test
@@ -44,4 +73,11 @@ public class ActivityDAOTest extends BaseCommonsTestCase {
   public void testFindPartIsCommentedConvoPoster() {
     assertTrue(activityCommentDAO.findPartIsCommentedConvoPoster("john").isEmpty());
   }
+
+  @Override
+  protected void afterClass() {
+    super.afterClass();
+    RequestLifeCycle.end();
+  }
+
 }
